2a4a25c14ab0ba34e57e1accfac8055f087d15a2
Felipe Silva <felipe.silvadev898@gmail.com>
2021-03-28 12:23:16 -0300

    corrigindo testes

--- src/app/repository/repository.component.spec.ts
+++ src/app/repository/repository.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule} from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { RepositoryComponent } from './repository.component';
 
@@ -8,6 +10,8 @@ describe('RepositoryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[ HttpClientTestingModule,
+                MatSnackBarModule ],
       declarations: [ RepositoryComponent ]
     })
     .compileComponents();
